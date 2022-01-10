c7e3c2c673b0055fbbdfe2feb95dc42394bcf524
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2022-01-10 08:18:04 -0400

    att test

--- src/services/receita-service.spec.ts
+++ src/services/receita-service.spec.ts
@@ -4,14 +4,24 @@ import { ReceitaService } from './receita-service';
 describe('ReceitaService', () => {
   it('should create receita', async () => {
     const receitaService = new ReceitaService();
-    const receita = await receitaService.create('Bolo', 1, 'fogo a gosto', 8);
+    const receita = await receitaService.create(
+      'Bolo',
+      1,
+      'Asse em forno médio 180 °C',
+      8,
+    );
     expect(receita).toBeInstanceOf(Receita);
     expect(receita).toHaveProperty('nome', 'Bolo');
   });
 
   it('should not create receita if not found receita type', async () => {
     const receitaService = new ReceitaService();
-    const error = await receitaService.create('Bolo', 111, 'fogo a gosto', 8);
+    const error = await receitaService.create(
+      'Bolo',
+      111,
+      'Asse em forno médio 180 °C',
+      8,
+    );
     expect(error).toBeInstanceOf(Error);
   });
 });
@@ -31,13 +41,16 @@ it('should return undefined if not found a receita', async () => {
 
 it('should update the receita level', async () => {
   const receitaService = new ReceitaService();
-  const receita = await receitaService.update(345, 'sal a gosto');
+  const receita = await receitaService.update(
+    345,
+    'Asse em forno médio 180 °C',
+  );
   expect(receita).toBeUndefined();
 });
 
 it('should update the receita level (Error)', async () => {
   const receitaService = new ReceitaService();
-  const receita = await receitaService.update(-1, 'sal a ');
+  const receita = await receitaService.update(-1, 'Asse em forno médio 180 °C');
   expect(receita).toBeInstanceOf(Error);
 });
 
